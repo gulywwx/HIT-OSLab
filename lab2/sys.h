bb0d6b3c5991df09853f071876a904c40425d38a
Joey <gulyxxf@gmail.com>
2019-05-27 21:41:41 +1000

    Update sys.h

--- lab2/sys.h
+++ lab2/sys.h
@@ -70,8 +70,12 @@ extern int sys_sgetmask();
 extern int sys_ssetmask();
 extern int sys_setreuid();
 extern int sys_setregid();
+
+//add two declear
 extern int sys_iam();
 extern int sys_whoami();
+
+//add two item to method array
 fn_ptr sys_call_table[] = { sys_setup, sys_exit, sys_fork, sys_read,
 sys_write, sys_open, sys_close, sys_waitpid, sys_creat, sys_link,
 sys_unlink, sys_execve, sys_chdir, sys_time, sys_mknod, sys_chmod,
